be4e0df8122cbe52729c12e6e72556fe27740e6a
stefan.doerger <stefan.doerger@winlocal.de>
2022-01-29 14:40:24 +0100

    RM CMNT, #develop

--- pkg/copy/copyFiles.go
+++ pkg/copy/copyFiles.go
@@ -15,8 +15,6 @@ func CopyFiles(s int /* start index */, e int /* end index */, d string /* dir p
 	// s = START
 	// e = END
 
-	// fmt.Println("Current Index")
-	// fmt.Println(i)
 	fmt.Println(targetDir)
 
 	dn := "dir_" + strconv.Itoa(i+1) // dir name + index
@@ -45,16 +43,7 @@ func CopyFiles(s int /* start index */, e int /* end index */, d string /* dir p
 			// Get file name
 			fn := filepath.Base(original.Name())
 
-			fmt.Println("fn")
-			fmt.Println(fn)
-
 			// Create new file
-			// fmt.Println("d + targetDir + fn")
-			fmt.Println(d + "/" + targetDir + "/" + dn + "/" + fn)
-
-			fmt.Println("I RUN")
-
-			// TODO: CMT IN
 			new, err := os.Create(d + "/" + targetDir + "/" + dn + "/" + fn)
 			if err != nil {
 				log.Fatal(err)
